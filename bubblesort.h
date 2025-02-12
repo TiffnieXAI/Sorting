d34993ba9285ca3c04dfc6ee7011f36255a1a55d
Tiffnie Tisoy <wrstiffanytisoy@gmail.com>
2025-02-12 23:07:13 +0800

    Update bubblesort.h

---
+++ bubblesort.h
@@ -0,0 +1,7 @@
+#ifndef BUBBLESORT_H  // Prevents multiple inclusions
+#define BUBBLESORT_H
+
+// Function prototype for bubble sort
+void bubbleSort(int array[], int size);
+
+#endif // BUBBLESORT_H
